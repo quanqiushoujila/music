1b2dcf2f5bf39e3f92caf15f867cca28651cf1d3
kai <1079049816@qq.com>
2017-12-19 22:09:11 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}<span>2222</span></h1>
+    <h1>{{ msg }} <i class="icon-ok"></i></h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
