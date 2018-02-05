d3feef3332e6c86b3d990ba43621f8d5896f74f6
k <k@k.com>
2018-02-05 17:11:31 +0800

    singer

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,9 +1,26 @@
-// import jsonp from 'common/js/jsonp'
-// import { commonParams, options } from './config'
+import jsonp from 'common/js/jsonp'
+import { commonParams, options } from './config'
 import axios from 'axios'
 
-export function getSingerList () {
+export const getSingerList = () => {
   return axios.get('/api/singer').then((res) => {
     return Promise.resolve(res.data)
   })
+}
+export const getSingerDisc = () => {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    loginUin: 0,
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    singermid: '0025NhlN2yWrP4',
+    order: 'listen',
+    begin: 0,
+    num: 30,
+    songstatus: 1
+  })
+
+  return jsonp(url, data, options)
 }
--- src/pages/singer/singer.vue
+++ src/pages/singer/singer.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     歌手
-    {{list}}
   </div>
 </template>
 
 <script>
-import { getSingerList } from 'api/singer'
+import { getSingerList, getSingerDisc } from 'api/singer'
+import { ERR_OK } from 'api/config'
 export default {
   name: 'singer',
   data () {
@@ -17,11 +17,21 @@ export default {
   methods: {
     _getSingerList () {
       getSingerList().then((res) => {
-        this.list = res
+        res = res.data
+        if (res.code === ERR_OK) {
+          this.list = res.data
+        }
+      })
+    },
+    _getSingerDisc () {
+      getSingerDisc().then((res) => {
+        console.log('222', res)
       })
     }
   },
   created: function () {
+    this._getSingerList()
+    this._getSingerDisc()
   }
 }
 </script>
